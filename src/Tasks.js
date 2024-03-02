3e037e712dc2a3d3e78471a16bbeca84c34bc7b5
anujs6 <127349846+anujs6@users.noreply.github.com>
2024-03-01 22:14:51 -0500

    Updated Tasks Component

--- src/Tasks.js
+++ src/Tasks.js
@@ -1,1 +1,16 @@
+import React from "react";
+import * as dayjs from 'dayjs';
+import './App.css';
 
+export default function Tasks({tasks}) {
+    return tasks.map(task => (
+      <div key={task.id} className="task">
+        <h3>{task.name}</h3>
+        <p>Description: {task.description}</p>
+        <p>Start: {dayjs(task.start).format('YYYY-MM-DD HH:mm')}</p>
+        <p>End: {dayjs(task.end).format('YYYY-MM-DD HH:mm')}</p>
+        <p>Priority: {task.priority}</p>
+        <p>Status: {task.status}</p>
+      </div>
+    ));
+  }
