c7d459f37c640bda40433258123ac1bc2cd595c0
Ghosfreak12 <96085705+Ghosfreak12@users.noreply.github.com>
2024-03-08 23:44:56 -0500

    Update Tasks.js

--- src/Tasks.js
+++ src/Tasks.js
@@ -2,17 +2,16 @@ import React from "react";
 import * as dayjs from 'dayjs';
 import './App.css';
 
-export default function Tasks({tasks}) {
-    return tasks.map(task => (
-      <div key={task.id} className="task">
-        <h3>{task.name}</h3>
-        <p>Description: {task.description}</p>
-        <p>Start: {dayjs(task.start).format('YYYY-MM-DD HH:mm')}</p>
-        <p>End: {dayjs(task.end).format('YYYY-MM-DD HH:mm')}</p>
-        <p>Priority: {task.priority}</p>
-        <p>Status: {task.status}</p>
-        <button onClick={() => deleteTask(task.id)} className="button">Delete Task</button>
-        <button onClick={() => completeTask(task.id)} className="button">Mark as Completed</button>
-      </div>
-    ));
-  }
+export default function Tasks({ tasks, deleteTask, completeTask }) {
+  return tasks.map(task => (
+    <div key={task.id} className="task">
+      <h3>{task.description}</h3> 
+      <p>Start: {dayjs(task.start).format('YYYY-MM-DD HH:mm')}</p>
+      <p>End: {dayjs(task.end).format('YYYY-MM-DD HH:mm')}</p>
+      <p>Priority: {task.priority}</p>
+      <p>Status: {task.status}</p>
+      <button onClick={() => deleteTask(task.id)} className="button">Delete Task</button>
+      <button onClick={() => completeTask(task.id)} className="button">Mark as Completed</button>
+    </div>
+  ));
+}
