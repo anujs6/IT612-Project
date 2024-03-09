0977b751ce517b3185e36fc7f7f3f5bc5efea4c4
anujs6 <127349846+anujs6@users.noreply.github.com>
2024-03-08 21:19:37 -0500

    Button Component file, Needs to be tested

---
+++ src/Button.js
@@ -0,0 +1,9 @@
+import React from "react";
+import addTask, signOut from "./App.js";
+import './App.css';
+
+export default function Button (onClick, name) {
+  return (
+    <button onClick={onClick} className="button">{name}</button>
+  );
+}
